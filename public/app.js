341992e61753d9488765bf162a8e539a51131555
shuhaowang <wshuhao@gmail.com>
2017-10-01 00:16:16 -0700

    add webpack watch and debug, add babel-polyfill to support new features for browswer

---
+++ public/app.js
@@ -0,0 +1,16 @@
+webpackJsonp([0],{
+
+/***/ 32:
+/*!************************************!*\
+  !*** multi ./lib/renderers/dom.js ***!
+  \************************************/
+/*! no static exports found */
+/*! all exports used */
+/***/ (function(module, exports, __webpack_require__) {
+
+(function webpackMissingModule() { throw new Error("Cannot find module \"./lib/renderers/dom.js\""); }());
+
+
+/***/ })
+
+},[32]);
